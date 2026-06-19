8db1fe6154171a355269cc40e23e5e6edb003fa7
knantha475@gmail.com <knantha475@gmail.com>
2026-06-19 22:31:17 +0530

    added pwa support

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
@@ -7,6 +8,8 @@ export default defineConfig({
   plugins: [
 
     react(),
+
+    tailwindcss(),
 
     VitePWA({
 

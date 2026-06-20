4170fa674d31e92be63f1c4fa8b3939cdbccbcc2
knantha475@gmail.com <knantha475@gmail.com>
2026-06-20 10:11:47 +0530

    fix firebase admin version

--- Server/config/firebase.js
+++ Server/config/firebase.js
@@ -1,21 +1,21 @@
-const admin = require("firebase-admin");
+const admin = require("firebase-admin")
+console.log(admin)
 
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
-
-  clientEmail:
-    process.env.FIREBASE_CLIENT_EMAIL,
-
-  privateKey:
-    process.env.FIREBASE_PRIVATE_KEY
-      .replace(/\\n/g, "\n"),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(
+    /\\n/g,
+    "\n"
+  ),
 };
 
-admin.initializeApp({
-  credential:
-    admin.credential.cert(
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(
       serviceAccount
     ),
-});
+  });
+}
 
 module.exports = admin;

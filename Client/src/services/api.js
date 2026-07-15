eb60939750703f9322d2a14d676d963b87cad86d
knantha475@gmail.com <knantha475@gmail.com>
2026-07-15 20:59:22 +0530

    Restaurant selection and DT Cafe redesign

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-
-baseURL:
-"https://downtown-food-ordering-system.onrender.com/api",
-
+  baseURL: "https://downtown-food-ordering-system.onrender.com/api",
 });

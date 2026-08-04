98162916d39b5db4389a86ff90ce9b77cb0df6a8
knantha475@gmail.com <knantha475@gmail.com>
2026-08-04 19:50:48 +0530

    login probs

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-// export default axios.create({
-//   baseURL: "http://localhost:5000/api",
-// });
-
 export default axios.create({
-  baseURL: "https://downtown-food-ordering-system-1.onrender.com/api",
+
+  baseURL:
+    "https://downtown-food-ordering-system-1.onrender.com/api",
+
 });

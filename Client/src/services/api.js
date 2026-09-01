485c67f7d8783282b5d060bc84808865d8343aa0
knantha475@gmail.com <knantha475@gmail.com>
2026-09-01 18:51:27 +0530

    LOGO and PICS change

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,12 +1,12 @@
-// import axios from "axios";
-// export default axios.create({
-//   baseURL:
-//     "https://downtown-food-ordering-system-2.onrender.com/api",
-
-// });
-
 import axios from "axios";
-
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    "https://downtown-food-ordering-system-2.onrender.com/api",
+
 });
+
+// import axios from "axios";
+
+// export default axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
